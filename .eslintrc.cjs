6ef0d08d137f838557393b1c588fd7e15d329a81
Snarloff <snarloff@gmail.com>
2023-10-12 12:12:42 -0300

    chore(linter): :pushpin: adicionando versão correta para o eslint funcionar

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    '@snarloff/eslint-config',
+    '@snarloff/eslint-config/next-tailwind',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
@@ -11,6 +11,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'react/no-unknown-property': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
